4d48b6a5a03db0a03062adf177753e0d3667d478
Dieter Flick <dflick@pivotal.io>
2015-06-15 08:53:12 +0200

    first tile config added

---
+++ createTile.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TILE_NAME=sentimentr
+TILE_FILE=`pwd`/*tile.yml
+RELEASE_TARFILE=`pwd`/releases/*/*.tgz
+#BOSH_STEMCELL_FILE=`cat ${TILE_FILE} | grep "bosh-stemcell" | grep "^ *file:" | awk '{print $2}' `
+#BOSH_STEMCELL_LOCATION=https://s3.amazonaws.com/bosh-jenkins-artifacts/bosh-stemcell/vsphere
+
+mkdir -p tmp
+pushd tmp
+mkdir -p metadata releases #stemcells
+cp $TILE_FILE metadata
+cp $RELEASE_TARFILE releases
+#if [ ! -e "stemcells/$BOSH_STEMCELL_FILE" ]; then
+#  curl -k $BOSH_STEMCELL_LOCATION/$BOSH_STEMCELL_FILE -o stemcells/$BOSH_STEMCELL_FILE
+#fi
+zip -r $TILE_NAME.pivotal metadata releases #stemcells
+mv $TILE_NAME.pivotal ..
+popd
