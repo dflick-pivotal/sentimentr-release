20a1e360be8f0918d72d859c6e3492f11e09e6be
Dieter Flick <dflick@pivotal.io>
2015-06-18 15:07:18 +0200

    createRelease.sh

--- createRelease.sh
+++ createRelease.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-# For use to create final release tarball
-CREATE_FINAL_TARBALL=true
-
 echo "Cleanup previous release ..."
 rm -rf releases/sentimentr-release/*.tgz
 
 echo "Creating the release ..."
 bosh create release --force
 
-if [ "$CREATE_FINAL_TARBALL" == "true" ]; then
-  # To create a final tarball release
-  bosh -n create release --with-tarball --final --force
-fi
+bosh -n create release --with-tarball --final --force
 
 echo "Done creating the release ..."
